7f100042694f789ece9e41300cdd21e1f6fd767d
iMac 27 <iMac@macbook-pro-de-rogers.local>
2016-02-03 18:03:09 -0300

    Initial Commit

---
+++ prueba GithubTests/prueba_GithubTests.swift
@@ -0,0 +1,36 @@
+//
+//  prueba_GithubTests.swift
+//  prueba GithubTests
+//
+//  Created by iMac 27 on 03-02-16.
+//  Copyright © 2016 iMac 27. All rights reserved.
+//
+
+import XCTest
+@testable import prueba_Github
+
+class prueba_GithubTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
